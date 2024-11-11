09b0fb624c886d202df6fec750d6f6c617b71c36
GustavoLima67 <lgusta333@gmail.com>
2024-11-11 18:44:23 -0300

    Começo da estilização da ultima pagina / sala

--- src/app/examples/moderadores/page.tsx
+++ src/app/examples/moderadores/page.tsx
@@ -3,7 +3,22 @@ import "./style.css"
 const Moderadores = () => {
     return(
         <div className="body">
-            <h1>Hello World</h1>
+            <div className="main">
+                <div className="card-main">
+                    <h1>BEM VINDO A SALA DE CADASTRO DE MODERADORES</h1>
+                </div>
+                <div className="cards-mains">    
+                    <div className="card-cadastre">
+                        <h1>CADASTRAR NOVO MODERADOR</h1>
+                    </div>
+                    <div className="card-moders-gifts">
+                        <h1>MODERADORES PRESENTES</h1>
+                    </div>
+                    <div className="card-moders-inserted">
+                        <h1>MODERADORES INSERIDOS</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
